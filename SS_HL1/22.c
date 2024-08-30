940e8825a51b391c9e90daed260c4a7de2389308
Rajan Khade <rajankhade31@gmail.com>
2024-08-30 11:03:00 +0530

    22.c small fix

--- SS_HL1/22.c
+++ SS_HL1/22.c
@@ -12,30 +12,43 @@ Write a program, open a file, call fork, and then write to the file by both the 
 #include<unistd.h>
 #include<fcntl.h>
 #include<sys/types.h>
+#include <sys/wait.h>
 
 int main(){
     int fd = open("test_dir_01/test_file_04.txt", O_CREAT | O_RDWR, 0666);
-    if(fd==-1) {
+    if(fd == -1) {
         perror("Error opening file");
         return 1;
     }
 
     int pid = fork();
-    if(pid==0){
-        char buf[]="Written from Child process\n";
-        int wr  = write(fd, &buf, sizeof(buf));
-        if(wr<0){
+
+    if(pid < 0) {
+        perror("Error creating child");
+    } else if(pid==0) {
+        char buff[] = "Written from child process\n";
+
+        int w  = write(fd, &buff, sizeof(buff)-1);
+        if(w<0) {
           perror("Cant write using child process\n");
           return 1;
         }
-    }
-    else{
-        char bf[]="Written from parent process\n";
-        int wr  = write(fd,&bf,sizeof(bf));
-        if(wr<0){
-          perror("Cant write using child process\n");
+    } else{
+        char buff[] = "Written from parent process\n";
+
+        int w  = write(fd,&buff,sizeof(buff)-1);
+        if(w<0) {
+          perror("Cant write using parent process\n");
+
+          wait(NULL);
+          close(fd);
+
           return 1;
         }
+
+        wait(NULL);
+        close(fd);
     }
+
     return 0;
 }
